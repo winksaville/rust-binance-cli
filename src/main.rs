847d2694ff88755dcc98ef843bc824e1515b50ed
Wink Saville <wink@saville.com>
2021-04-13 11:15:16 -0700

    Adding some command line processing
     - Require secret_key and api_key either as params or environment
       variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Debug, Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Cli {
+    #[clap(short, long, env = "SECRET_KEY")]
+    secret_key: String,
+
+    #[clap(short, long, env = "API_KEY")]
+    api_key: String,
+
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    #[allow(unused)]
+    let args = Cli::parse();
+
+    #[allow(unused)]
+    let sec_key: Vec<u8> = args.secret_key.as_bytes().to_vec();
+    let api_key: Vec<u8> = args.api_key.as_bytes().to_vec();
+    println!(
+        "sec_key=secret key is never displayed api_key={}",
+        std::str::from_utf8(&api_key).unwrap(),
+    );
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -4,11 +4,57 @@ use std::error::Error;
 use std::process::Command;
 
 #[test]
-fn test_hello_world() -> Result<(), Box<dyn Error>> {
+fn test_no_params() -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("binance-auto-sell")?;
 
     cmd.assert()
-        .stdout(predicate::str::contains("Hello, world!"));
+        .code(predicate::eq(2))
+        .stderr(predicate::str::contains(
+            "required arguments were not provided",
+        ));
+
+    Ok(())
+}
+
+#[test]
+fn test_help() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("binance-auto-sell")?;
+    cmd.arg("-h");
+
+    cmd.assert()
+        .code(predicate::eq(0))
+        .stdout(predicate::str::contains("USAGE"));
+
+    Ok(())
+}
+
+#[test]
+fn test_req_params() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("binance-auto-sell")?;
+    cmd.arg("-s=secret_key");
+    cmd.arg("-a").arg("api key");
+
+    cmd.assert()
+        .code(predicate::eq(0))
+        .stdout(predicate::str::contains(
+            "sec_key=secret key is never displayed",
+        ))
+        .stdout(predicate::str::contains("api_key=api key"));
+
+    Ok(())
+}
+#[test]
+fn test_req_params_as_env_vars() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("binance-auto-sell")?;
+    cmd.env("SECRET_KEY", "secret key");
+    cmd.env("API_KEY", "api key");
+
+    cmd.assert()
+        .code(predicate::eq(0))
+        .stdout(predicate::str::contains(
+            "sec_key=secret key is never displayed",
+        ))
+        .stdout(predicate::str::contains("api_key=api key"));
 
     Ok(())
 }
